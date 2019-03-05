5ef84d438ee0efab700a11c9f86484595c41bb2c
YOUR NAME <allisongoins@my.unt.edu>
2019-03-04 18:27:33 -0600

    Allison: edited models.py so that the prereq and course classes would return descriptive names like MATH 1650: Pre-Calculus

--- src/courses/models.py
+++ src/courses/models.py
@@ -19,14 +19,17 @@ class Course(models.Model):
     description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return (self.courseDept + " " + str(self.courseID) + ": " +  self.name)
 
 class Prereq(models.Model):
     courseID = models.PositiveSmallIntegerField()
     courseDept = models.CharField(max_length=4)
     name = models.CharField(max_length=100, default='prereq course name')
+
+    #Note: When filling in the table, we list MATH 1650 as the course and prereq MATH 1710, which makes it look like its saying that MATH 1710 is a prereq for MATH 1650, but we do that because one single course is the prereq to to another but a course may have multiple prereqs.
     prereqCourseID = models.PositiveSmallIntegerField()
     prereqCourseDept = models.CharField(max_length=4)
 
     def __str__(self):
-        return self.name
+        return self.courseDept + " " + str(self.courseID) + " -> " + self.prereqCourseDept + " " +  str(self.prereqCourseID)
+        
