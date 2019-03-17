0da5a43fce01ed19ccc133a020a04143d540cc1c
JesusRodriguezUNT <32721119+JesusRodriguezUNT@users.noreply.github.com>
2019-03-16 19:20:09 -0500

    Added more comments to the function in utils.py

--- src/degrees/utils.py
+++ src/degrees/utils.py
@@ -31,51 +31,73 @@ def timelineGenerator():
     # return a dictionary that acts like a hash map
     return timeline
 
+# Description: A function used to find a course in the timeline
+# Return:      The function returns the location of the course in the timeline and a boolean
+#              representing whether it exists in the timeline or not
+# Parameters:  course is a string of course such as CSCE 1030
+#              timeline is a dictionary where each key holds a list of tuples
+#              Ex. {1:[(CSCE 1030, 0, [])]}
 def findCourse(course, timeline):
-    index = 0
-    placed = False
+    index = 0 # the key of the timeline dictionary
+    placed = False # assume course is not in the timeline
 
     #print(timeline) # test print
+
+    # loop through each entry in the the timeline
     for i in timeline:
-        innerIndex = 0
+        innerIndex = 0 # the index of the course at key index
+        
+        # if the key is an empty array
         if not timeline[i]:
             continue
+        
+        # for each entry in the key
         for j in timeline[i]:
+            # if the course is in the timeline
             if j[0] == course:
-                placed = True
+                placed = True 
                 break
-            innerIndex = innerIndex + 1
+
+            innerIndex = innerIndex + 1 # move the index forward
+        
+        # if the course is in the timeline
         if placed == True:
-             break
+            break
+        # else increase the index
         else: 
             index = index + 1
 
     return (placed, index, innerIndex)
 
+# Description: This function updates the second entry in a tuple.
+#              Ex. (CSCE 1030, 0, []) this function would increase 0 to 1
+# Return:      The index of the course, i.e. the key
+# Parameters:  updateInfo is a tuple with a boolean value, a dictionary key, and a list index
+#                   Ex. (True, 2, 3)
+#              course is a string such as CSCE 1030
+#              timeline is a dictionary of all the courses in the timeline
 def updatePriority(updateInfo, course, timeline):
-    index = updateInfo[1]
-    innerIndex = updateInfo[2]
+    index = updateInfo[1] # the key in timeline of course
+    innerIndex = updateInfo[2] # the index in the list of key
 
-    priority = timeline[index][innerIndex][1]
-    prereqs = timeline[index][innerIndex][2]
+    priority = timeline[index][innerIndex][1] # the courses priority
+    prereqs = timeline[index][innerIndex][2] # the courses prereqs
 
-    temp = (course, priority + 1, prereqs)
+    temp = (course, priority + 1, prereqs) # make a new tuple
 
-    timeline[index] = [entry for entry in timeline[index] if entry[0] != course]
-    timeline[index].append(temp)
+    timeline[index] = [entry for entry in timeline[index] if entry[0] != course] # build a new list without the course
+    timeline[index].append(temp) # add the tuple to the list
 
-    return index
+    return index # return the index
 
 #**** no longer using the offset
 #**** I should probably add a list of the prereqs to the tuple in the timeline
 # needs the course, the course list, the corelist, the "offset", the dictionary
 def coursePlacement(currentCourse, coursesList, coreList, offset, timeline):
     print("++++++++++ Current Course: " + currentCourse)
-    #print(coursesList)
     tokens = currentCourse.split()
     
     checkCourse = Course.objects.filter(courseDept=tokens[0], courseID=tokens[1]).exists()
-    #print(checkCourse)
 
     if not checkCourse:
         print("The course does not exists. Return an error.")
@@ -83,9 +105,7 @@ def coursePlacement(currentCourse, coursesList, coreList, offset, timeline):
 
     # here we get the prereqs for a course in the courses list with a database call
     try:
-    #    tokens = currentCourse.split()
         prereqs = Prereq.objects.filter(courseDept=tokens[0], courseID=tokens[1])
-
 
         # if the course does not have prereqs
         if not prereqs:
@@ -113,21 +133,15 @@ def coursePlacement(currentCourse, coursesList, coreList, offset, timeline):
             prereqArr = []
 
             temp = model_to_dict(prereqs[0])
- #           print("test 1")
 
             largest = 0
-#            print("priting prereqs: ", end='')
-#            print(temp)
+
             # for each prereq of currentCourse
             for i in temp["prereqCourses"]:
-                #print("***** Current prereq:  "+temp["prereqCourses"][i][0]) # test print
-                #print(i)
-
                 # check if the prereq is a core course
                 prereqName = temp["prereqCourses"][i][0]
                 pTokens = prereqName.split()
                 prereqCat = list(Course.objects.filter(courseDept=pTokens[0], courseID=pTokens[1]).values('category'))
-                #print(prereqCat[0]['category']) # test print
 
                 if not prereqCat:
                     print("empty list. The course isn't in the database. Must return error.")
@@ -144,13 +158,10 @@ def coursePlacement(currentCourse, coursesList, coreList, offset, timeline):
                     # ignore corerequisites since they can be taken at the same time
                     if i != 'C':
                         prereqArr.append(prereqName)
-                    #print("Added Prereq: " + prereqName)
-
 
                     # recursively update the prereq's prereqs
                     placement = coursePlacement(temp["prereqCourses"][i][0], coursesList, coreList, 0, timeline)+1
                     
-                    #print(placement) # test print
                     # keep track of the largest placement since it determines the placement of the current course
                     if placement > largest:
                         largest = placement
@@ -174,8 +185,6 @@ def coursePlacement(currentCourse, coursesList, coreList, offset, timeline):
                 # else the current course is not a leaf
                 else:
                     tempCourse = (currentCourse, 0, prereqArr)
-                    #print("here are all the prereqs for" + currentCourse+ " : ", end='')
-                    #print(prereqArr)
 
                     # check if the largest placement is a valid key in the timeline dictionary
                     if largest not in timeline:
@@ -192,19 +201,21 @@ def coursePlacement(currentCourse, coursesList, coreList, offset, timeline):
 
     return 0
 
+# Description: a function used with python's list sort. The function takes a tuple parameter
+# and uses it's second value to sort all tuples in a list
 def priority(x):
     return x[1]
 
 # this function needs to divide the timeline into the courses for each semester
 def processTimeline(timeline):
-    #print(timeline)
-    fullTimeline = []
+    fullTimeline = [] # the processed timeline
 
-    tempArr = []
-    index = 0
-    counter = 0
-    total = 0
+    tempArr = [] # an array to append to the processed timeline
+    index = 0 # the index of the timeline
+    counter = 0 # a variable to count in fives
+    total = 0 # the total of courses in level
 
+    # while there are entries in the timeline to process
     while True:
         # if the current timeline level is not empty
         if timeline[index]:
@@ -214,16 +225,20 @@ def processTimeline(timeline):
         else: 
             index = index + 1
 
-            # copy of code below
-            if index == len(timeline):
-                break
-        counter = counter + 1
-        total = total + 1
+        # check if the index is still valid
+        if index == len(timeline):
+            break
+        
+        counter = counter + 1 # move to the next entry in the current semester
+        total = total + 1 # move to the next course
 
+        # if the semester is full
         if counter == 5:
-            counter = 0
-            fullTimeline.append(tempArr)
-            tempArr = []
+            counter = 0 # reset the counter
+            fullTimeline.append(tempArr) # add the current semester to the timeline
+            tempArr = [] # clear the contents of the temporary array
+
+        # if there are no more courses in the current level
         if total == len(timeline[index]):
 
             # if there are spots that can be filled in the current semester
@@ -231,19 +246,14 @@ def processTimeline(timeline):
 
                 # loop through the next course level
                 for i,j,k in timeline[index + 1]:
-                    print(i, end=' prereqs: ')
-                    print(k)
                     canAdd = True # assume that the current course can be added to the current semester
 
                     # loop through the prereqs of courses in level + 1
                     for prereq in k:
                         # if the prereq is in the current semester
                         if prereq in tempArr:
-                            print("can't add: " + i)
-                            canAdd = False
-                        # else the course can be added to the current semester
-                        #else:
-                        #    canAdd = True 
+                            # print("can't add: " + i) # test print
+                            canAdd = False # can't add this course
                             
                     # if the course can be added to the timeline
                     if canAdd:
@@ -251,21 +261,24 @@ def processTimeline(timeline):
                         timeline[index+1].remove((i,j,k)) # remove the course from the timeline at level + 1
                         counter = counter + 1 # increase the counter
                         
-                        if counter == 5:
+                        # if there are no more course spaces in the current semester
+                        if counter == 5: 
                             break
-            fullTimeline.append(tempArr)
+
+            fullTimeline.append(tempArr) # add the semester to the timeline
+            index = index + 1 # move to the next level in the timeline
+
+            # reset variable
             tempArr = []
-            total = 0
-            index = index + 1
+            total = 0 
             counter = 0
 
-        # this statement should be moved to the very top
+        # check if the index is still valid
         if index == len(timeline):
             break
-    print(tempArr)
+    
+    # if the semester has courses 
     if tempArr:
-        fullTimeline.append(tempArr)
+        fullTimeline.append(tempArr) # add the semester to the timeline
 
-    print("\n\n")
-    print(fullTimeline)
-    return fullTimeline
+    return fullTimeline # return the processed timeline
