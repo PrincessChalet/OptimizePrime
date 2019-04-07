3533dc93f65a7e18268b821993d0710c8bc9a9a3
JesusRodriguezUNT <32721119+JesusRodriguezUNT@users.noreply.github.com>
2019-04-06 23:23:53 -0500

    transfer draft code

--- src/transferCredits/models.py
+++ src/transferCredits/models.py
@@ -5,7 +5,7 @@ class TransferCredit(models.Model):
     courseID = models.PositiveSmallIntegerField(null=True, blank=True)
     courseDept = models.CharField(max_length=4,null=True, blank=True)
     name = models.CharField(max_length=50)
-    equivalentToID = models.PositiveSmallIntegerField()
+    equivalentToID = models.CharField(max_length=4)
     equivalentToDept = models.CharField(max_length=4)
     requiredScore = models.CharField(max_length=5, null=True, blank=True)
 
